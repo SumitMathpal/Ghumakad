a8ecc06c9252e1f0ced3df2281ed0e46b090d47e
SumitMathpal <sumitmathpalcse@gmail.com>
2025-07-30 14:08:28 +0530

    change port 3000 to 8080

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
   res.status(status).render("error.ejs", { message });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ||  8080;
 
 app.listen(port, () => {
   console.log("Connection successfully started on port", port);
